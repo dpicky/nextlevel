848a0296977d999a41b126b6061efa0fdf10be6f
David Pickering <david@justcloudit.co>
2024-09-07 10:34:34 +0100

    Build tweaks

--- hosting/settings/buildSettings.ts
+++ hosting/settings/buildSettings.ts
@@ -1,30 +1,30 @@
+import { BuildSpec } from 'aws-cdk-lib/aws-codebuild';
 import { environmentVariables } from "./environmentVariables";
 
-export const buildSettings = {
+export const buildSpec = BuildSpec.fromObjectToYaml({
   version: '1.0',
   applications: [
     {
-      appRoot: 'frontend/next-app',
       frontend: {
         phases: {
           preBuild: {
             commands: [
+              // Install the correct Node version, defined in .nvmrc
               'nvm install',
-              'nvm use',
-              // Avoid out of memory error. See https://cloudnamaste.com/amplify-console-build-fail-java-heap-out-of-memory/
+              'nvm use v20',
+              // Avoid memory issues with node
               'export NODE_OPTIONS=--max-old-space-size=8192',
-              'npm install -g pnpm',
-              // Ensure node_modules in the monorepo root folder are included in the artifacts
-              'pnpm install --virtual-store-dir frontend/next-app/node_modules/.pnpm',
+              // Ensure node_modules are correctly included in the build artifacts
+              'npm install',
             ],
           },
           build: {
-            commands: [          
-            // Allow Next.js to access environment variables
-            // See https://docs.aws.amazon.com/amplify/latest/userguide/ssr-environment-variables.html
-            `env | grep -E '${Object.keys(environmentVariables).join('|')}' >> .env.production`,
-            // Build Next.js app
-            'pnpm build --no-lint',
+            commands: [
+              // Allow Next.js to access environment variables
+              // See https://docs.aws.amazon.com/amplify/latest/userguide/ssr-environment-variables.html
+              `env | grep -E '${Object.keys(environmentVariables).join('|')}' >> .env.production`,
+              // Build Next.js app
+              'npm next build --no-lint',
             ],
           },
         },
@@ -35,4 +35,4 @@ export const buildSettings = {
       },
     },
   ],
-};
+});
